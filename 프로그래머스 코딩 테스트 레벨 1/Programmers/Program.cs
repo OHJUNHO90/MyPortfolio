94bc72a24574a7c2d1c25a40f454a4602e997fd0
{OHJUNHO90} <{wnsgh4221@daum.net}>
2022-06-13 10:19:22 +0900

    코딩테스트 체육복 작성 코드 커밋

--- 프로그래머스 코딩 테스트 레벨 1/Programmers/Program.cs
+++ 프로그래머스 코딩 테스트 레벨 1/Programmers/Program.cs
@@ -9,9 +9,41 @@ namespace Programmers
     {
         static void Main(string[] args)
         {
+            
 
         }
 
+        #region 체육복
+        //solution(7, new int[] { 4,3,2 }, new int[] { 1,6,5 });
+        //static int solution(int n, int[] lost, int[] reserve)
+        //{
+        //    // n 전체 학생수
+        //    // lost 도난당한 학생들 번호
+        //    // 여벌의 체육복을 가져온 학생들 번호
+        //    // -규칙 여벌의 체육복을 가져온 학생의 번호 +1 또는 -1만 체육복을 빌려줄수 있음
+        //    // 여벌의 체육복을 가져온 학생이 체육복을 도난당했을수도 있음, 만약 도난당했다면 체육복을 빌려줄수 없음
+        //    int answer = n;
+        //    int[] temp = reserve.Except(lost).ToArray();
+        //    lost = lost.Except(reserve).OrderBy(t => t).ToArray();
+        //    List<int> list = (reserve = temp).ToList();
+
+        //    for (int i = 0; i < lost.Length; i++)
+        //    {
+        //        if (list.Contains(lost[i] - 1))
+        //        {
+        //            answer++;
+        //            list.Remove(lost[i] - 1);
+        //        }
+        //        else if (list.Contains(lost[i] + 1))
+        //        {
+        //            answer++;
+        //            list.Remove(lost[i] + 1);
+        //        }
+        //    }
+
+        //    return answer - lost.Length;
+        //}
+        #endregion
         #region 모의고사
         // solution(new int[] { 1, 3, 2, 4, 2 });
         //static int[] solution(int[] answers)
