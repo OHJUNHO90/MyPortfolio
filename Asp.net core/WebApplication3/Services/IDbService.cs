580e53548aefb1e0ec0c0a6e85cab7df5b2f2594
User <wnsgh4221@daum.net>
2023-05-01 16:13:48 +0900

    미완성본 웹 프로젝트 제거

--- Asp.net core/WebApplication3/Services/IDbService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication3.Services
-{
-    public interface IDbService
-    {
-        Task<T?> GetAsync<T>(string command, object parms);
-        Task<List<T>> GetAll<T>(string command);
-        Task<int> EditData(string command, object parms);
-    }
-}
